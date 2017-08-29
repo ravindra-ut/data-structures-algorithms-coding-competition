0d685fe3b2d2c5b0f402930b7d99a68c4ca2cc70
Ravindra Manjunatha <ravindra@utexas.edu>
2017-08-28 21:39:32 -0500

    when duplicate elements exist

--- HashMap/TwoSum.java
+++ HashMap/TwoSum.java
@@ -2,15 +2,12 @@ class TwoSum {
   public int[] twoSum(int[] nums, int target) {
     Map<Integer, Integer> valueToIndex = new HashMap<>();
     for(int i=0; i < nums.length; i++){
-      valueToIndex.put(nums[i], i);
       int remainder = target - nums[i];
-      if(remainder == nums[i]){
-        continue;
-      }
       if(valueToIndex.containsKey(remainder)){
         int [] result = {valueToIndex.get(remainder), i};
         return result;
       }
+      valueToIndex.put(nums[i], i);
     }
     throw new IllegalArgumentException("No two sum found");
   }
